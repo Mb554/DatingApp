1bd1b9585be4d3e60a6354833702127c2cef61d5
SimphiweMthembu <2018736260@ufs4life.ac.za>
2023-03-22 01:49:38 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
 });
+builder.Services.AddCors();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
