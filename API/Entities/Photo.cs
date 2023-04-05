0a6371dc46884ba3cf16505975a6d0638a1e0f7d
SimphiweMthembu <2018736260@ufs4life.ac.za>
2023-04-05 02:47:15 +0200

    End of section 8

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace API.Entities
+{
+    [Table("Photos")]
+    public class Photo
+    {
+        public int Id{get;set;}
+        public String Url {get;set;}
+        public bool IsMain {get;set;}
+        public string PublicId {get;set;}
+
+        public int AppUserId {get;set;}
+        public AppUser AppUser{get;set;}
+
+    }
+}
