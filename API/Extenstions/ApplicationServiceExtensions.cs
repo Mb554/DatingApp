940a5edbd9a7e571c0e14b4de23834fd1fc3a393
SimphiweMthembu <2018736260@ufs4life.ac.za>
2023-05-02 11:28:39 +0200

    End of section 15

--- API/Extenstions/ApplicationServiceExtensions.cs
+++ API/Extenstions/ApplicationServiceExtensions.cs
@@ -24,6 +24,7 @@ namespace API.Extenstions
             services.AddScoped<IPhotoService,PhotoService>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository,LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
 
             return services;
 
